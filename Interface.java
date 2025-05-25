222a0a668739784a61fc7dea068ee857244906ad
Ziaul Hoque Bhuiyan <bhuiyanziaul75@gmail.com>
2025-05-25 20:04:00 +0600

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,9 @@
+
+public interface AirlineOperations {
+    void searchFlights();
+    void bookFlight();
+    void makePayment();
+    void issueTicket();
+    void manageFlights();
+    void viewSortedBookings();
+}
